e2de23b0d22de0de700d71cc77ccb25470791153
yashaswini <yashaswiniraiker20@gmail.com>
2023-07-01 19:17:39 +0530

    Initial commit

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import function
 
 todos = function.get_todos()
 
+st.set_page_config(layout="wide")
+
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -12,7 +14,12 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("this is a todo app")
-st.write("this will help you to increase your productivity")
+st.write("this will help you to increase your <b>productivity</b>",
+         unsafe_allow_html=True)
+
+st.text_input(label="ADD TODO HERE",placeholder="Add new todo...",
+                      on_change=add_todo, key="new_todo")
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,5 +27,4 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         function.write_todos(todos)
 
-value = st.text_input(label="ADD TODO HERE",placeholder="Add new todo...",
-                      on_change=add_todo, key="new_todo")
+
